485cc4fbc579e4da0f3b652a3e980bc216e29da7
Anh Le <kunquan2345@gmail.com>
2021-07-20 04:32:32 -0700

    Set up Strapi with post and user

---
+++ blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f70016deb4e25d79c4020b414f5138aa'),
+    },
+  },
+});
