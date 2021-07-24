f41cf5aabe007113d90aeb19e4dec48f8e9ae42e
Anh Le <kunquan2345@gmail.com>
2021-07-24 04:56:17 -0700

    Add Material-UI and set up it with NextJS

--- blog-next/pages/_app.tsx
+++ blog-next/pages/_app.tsx
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // *The useEffect part is needed for using Material-UI with NextJS
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+
+    if (jssStyles) {
+      // We kind of by-pass typescript check here since we want to delete the element itself
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ blog-next/pages/_document.tsx
@@ -0,0 +1,43 @@
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import Document, { Html, Main, NextScript } from "next/document";
+import React from "react";
+
+/**
+ * * This file _document.tsx is used to enable SSR for Material-UI.
+ * It is used in conjunction with some modification in _app.tsx
+ * Link: https://github.com/mui-org/material-ui/tree/master/examples/nextjs
+ */
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
