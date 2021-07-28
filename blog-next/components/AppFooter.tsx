871b5430cde2d613b9374252f4d0780ea63d102e
Anh Le <kunquan2345@gmail.com>
2021-07-28 03:43:32 -0700

    Add external link to GitHub, Twitter and Stackoverflow onto footer

--- blog-next/components/AppFooter.tsx
+++ blog-next/components/AppFooter.tsx
@@ -5,12 +5,53 @@ const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(5),
     padding: theme.spacing(2),
-    backgroundColor: "#907373",
+    backgroundColor: "#000",
+  },
+  externalLink: {
+    color: "#fafafa",
+    transition: "all 0.3s ease-in-out 0s",
+    textDecoration: "none",
+    marginLeft: theme.spacing(3),
+    fontSize: "1rem",
   },
 }));
 
 export default function AppFooter() {
   const classes = useStyles();
 
-  return <footer className={classes.root}>Developed by Anh Le 2021</footer>;
+  return (
+    <footer className={classes.root}>
+      {/**
+       * rel="noopener noreferrer" used with target="_blank" to mitigate a security risk
+       * Link: https://www.freecodecamp.org/news/how-to-use-html-to-open-link-in-new-tab/
+       */}
+      <a
+        href="https://twitter.com/jrgocode"
+        className={classes.externalLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      <span className={classes.externalLink}>•</span>
+      <a
+        href="https://github.com/AnnLe4869"
+        className={classes.externalLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
+      <span className={classes.externalLink}>•</span>
+
+      <a
+        href="https://stackoverflow.com/users/9087143/kunquan"
+        className={classes.externalLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        StackOverflow
+      </a>
+    </footer>
+  );
 }
